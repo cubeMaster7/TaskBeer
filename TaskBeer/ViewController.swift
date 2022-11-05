974841f2e975b73ebb3cbc0bfc43bb28d4b61b62
cubeMaster7 <ienglishman187@gmail.com>
2022-11-05 22:55:07 +0300

    Initial Commit

---
+++ TaskBeer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskBeer
+//
+//  Created by Paul James on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
